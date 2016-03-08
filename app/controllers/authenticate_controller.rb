61f7f241598df5df7c8831fe9736d8f1069042f0
damianhouse <damianhouse@gmail.com>
2016-03-08 17:26:54 -0500

    Move notices from index pages into the application layout.

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -1,7 +1,14 @@
 class AuthenticateController < ApplicationController
   def login
+    if request.post?
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        User.find_by(email: params[:email])
+        flash[:notice] = “Whatever you want it to flash at this point”
   end
 
   def logout
+    session[:user_id] = nil
+    redirect to authenticate_login_path, notice: “Whatevs”
   end
 end
